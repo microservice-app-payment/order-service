c676022cb03cb300a65896e15f1c2b613e42cda9
ngochung1809 <ngochung1809@kali.kali>
2023-09-01 17:04:26 +0700

    update okta client

--- src/main/java/com/vn/config/RestTemplateConfig.java
+++ src/main/java/com/vn/config/RestTemplateConfig.java
@@ -1,15 +1,38 @@
 package com.vn.config;
 
+import com.vn.external.intercept.RestTemplateInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @Configuration
 public class RestTemplateConfig {
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+    @Autowired
+    private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
+
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate(){
-        return new RestTemplate();
+        RestTemplate restTemplate =  new RestTemplate();
+
+        restTemplate.setInterceptors(
+                Arrays.asList(
+                        new RestTemplateInterceptor(
+                                OAuth2AuthorizedClientManagerConfig.clientManager(clientRegistrationRepository,oAuth2AuthorizedClientRepository)
+                        )
+                )
+        );
+
+        return restTemplate;
+
     }
 }
--- src/main/java/com/vn/controller/OrderController.java
+++ src/main/java/com/vn/controller/OrderController.java
@@ -7,6 +7,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class OrderController {
     @Autowired
     OrderService orderService;
 
+    @PreAuthorize("hasAnyAuthority('Cutomer')")
     @PostMapping("/placeOrder")
     public ResponseEntity<Long> placeOrder(
         @RequestBody OrderRequest orderRequest
@@ -30,6 +32,7 @@ public class OrderController {
         return new ResponseEntity<>(orderId, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyAuthority('Admin') || hasAnyAuthority('Customer')")
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderResponse> getOrderDetails(
         @PathVariable(value = "orderId") Long orderId
